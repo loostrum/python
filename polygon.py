d25d6f55845d96364fbd91d4eb5a63b78346e917
Leon Oostrum <leonoostrum@gmail.com>
2014-01-16 12:40:07 +0100

    pi is not needed

--- polygon.py
+++ polygon.py
@@ -1,15 +1,13 @@
 from swampy.TurtleWorld import *
-from math import pi
 
 world=TurtleWorld()
 bob=Turtle()
 
 
-def polygon(t, n, r):
+def polygon(t, n, l):
 	angle=360./n
-	length=2*pi*r/n
 	for i in range(n):
-		fd(t, length)
+		fd(t, l)
 		lt(t, angle)
 
 r=100
@@ -17,6 +15,6 @@ input=raw_input('How many sides do you want?\n')
 n=int(input)
 
 print 'Generating an',n,'sided polygon using turtle',bob,'...'
-polygon(bob,n,r)
+polygon(bob,n,50)
 print 'Done!'
 wait_for_user()
