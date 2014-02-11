4c60f0315474e2853fa0bf431e573b6069e21480
Leon Oostrum <leon@zeus>
2014-02-11 19:57:32 +0100

    workshop files moved to own folder

--- README.md
+++
@@ -1,4 +0,0 @@
-python
-======
-
-UvA Python workshop
--- parsing.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-argparser=argparse.ArgumentParser(description='Process some ints.')
-argparser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
-argparser.add_argument('--sum', dest='accumulate',action='store_const', const=sum, default=max, help='sum the integers (default:find the max)')
-args=argparser.parse_args()
-print args.accumulate()
-
