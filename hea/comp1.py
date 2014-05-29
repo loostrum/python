24d5ff094a3f1a416ba6b468d585eecf096325f3
Leon Oostrum <leonoostrum@gmail.com>
2014-05-29 12:43:54 +0200

    mb distribution added need maxwell-juttner?

--- hea/comp1.py
+++ hea/comp1.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 from random import uniform
 from matplotlib import pyplot as plt
-import scipy.stats as stats
+from scipy.stats import itemfreq
 from time import time
 import argparse
 
@@ -58,30 +58,6 @@ def e_el(mu_l,beta,gamma):
 #generate scattered photon energy in lab frame for given theta1_e (=scatter angle in e frame), relative to e_l
 def e1_la(mu1_e,e_e,beta,gamma):
     return e_e*gamma*(1+beta*mu1_e)
-    
-
-def gen_photon(beta,gamma):
-    #generate random photon direction
-    mu_l=rand_mu()
-    #calc incident energy in e frame
-    e_e=e_el(mu_l,beta,gamma)
-    #calc scattered energy in lab frame, assuming no recoil (e_e=e_1 in electron frame)
-    mu1_e=rand_mu()
-    mu1_l=mu_la(mu1_e,beta)
-    e_1=e1_la(mu1_e,e_e,beta,gamma)
-    #discard if impossible 
-    if e_1 < 1/(gamma**2*(1+beta)*(1-beta*mu1_l)) or e_1 > 1/(gamma*(1-beta)*(1-beta*mu1_l)):
-        #print 'discarded:',e_1
-        return 0
-    else:
-        #print 'approved'
-        return e_1
-        
-def get_electron():
-    #thermal distribution with 3kT/2 = B^2 /8pi -> kT = B^2 / 12 pi
-    # a := exp(-v^2/2a^2) = exp(-m v^2 / 2 kT) -> a = sqrt(kt/m) = sqrt(B^2/12 pi m)
-    maxwell = stats.maxwell(
-    
 
 
 def main():
@@ -90,33 +66,57 @@ def main():
     c=3E10
     E=1.6E-6
     gamma=E/(m*c**2)
+    #take energy from synchrotron distribution
+    
+    #gamma=10
     beta=math.sqrt(1-gamma**-2)
+
+
 
     print 'gamma, beta, emax/e: ',gamma,beta,(gamma*(1+beta))**2
     niter=int(10**args.N)
     print 'Producing 1E'+str(args.N)+' photons'
 
         
-    #initialize stuff
+        
     disc=0
-    e1_list=[]
-    ebins=np.asarray(list(frange(0,10,.1)))
-    
     tstart=time()
-          
+    #make empty list for final scattered energies
+    e1_list=[]
+    
     for i in range(niter):
-        tmp=gen_photon(beta,gamma)
-        if tmp == 0:
+        #generate uniform photon direction
+        mu_l=rand_mu()
+        #calc incident energy in e frame
+        e_e=e_el(mu_l,beta,gamma)
+        #calc scattered energy in lab frame, assuming no recoil (e_e=e_1 in electron frame)
+        mu1_e=rand_mu()
+        mu1_l=mu_la(mu1_e,beta)
+        e_1=e1_la(mu1_e,e_e,beta,gamma)
+        #discard if impossible 
+        if e_1 < 1/(gamma**2*(1+beta)*(1-beta*mu1_l)) or e_1 > 1/(gamma*(1-beta)*(1-beta*mu1_l)):
+            #print 'discarded:',e_1
             disc+=1
         else:
-            e1_list.append(tmp)
+            #print 'approved'
+            e1_list.append(e_1)
+
         if 100*float(i+1)/niter%5==0:
             print 100*(i+1)/niter, '%'
     
     tend=time()
-    
     print 'Time used for generating photons: ', round(tend-tstart,1), 's'
     
+ 
+
+    ebins=np.asarray(list(frange(0,10,.1)))
+    #ebins=list(frange(-3,1,.1))
+    #ebins = [ 10**item for item in ebins ]
+    
+    #dbin=.1
+
+    #e1_list=sorted([ dbin*round(item/dbin) for item in e1_list ])
+
  
     tstart=time()
    
@@ -132,18 +132,21 @@ def main():
             else:
                 finallist[i].append(item)
                 break
-                
-    finallist=[ float(len(elem))/niter for elem in finallist ]
-                          
+     
+    
+
+    
+                     
     tend=time()
     print 'Time used for binning: ', round(tend-tstart,1), 's'
     print 'discarded: ', 100*float(disc)/niter, '%'
     
 
     
-   
+    ylist=[ float(len(elem))/niter for elem in finallist ]
     plt.clf()
-    plt.plot(ebins,finallist)
+    #plt.loglog(ebins,ylist)
+    plt.plot(ebins,ylist)
     plt.xlabel('e1/e0')
     plt.ylabel('dn/de')
     plt.show()
