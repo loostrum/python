dcb508f08c2f0aed3dcfd3856a51575808c7fe7a
Leon Oostrum <leonoostrum@gmail.com>
2014-05-30 13:57:13 +0200

    added MJ distribution (work in progress)

--- hea/compton.py
+++ hea/compton.py
@@ -3,6 +3,7 @@ import numpy as np
 from random import uniform
 from matplotlib import pyplot as plt
 import scipy.stats as stats
+from scipy.integrate import quad
 from time import time
 import argparse
 
@@ -77,25 +78,50 @@ def gen_photon(beta,gamma):
         #print 'approved'
         return e_1
         
-def get_electron():
-    #thermal distribution with 3kT/2 = B^2 /8pi -> kT = B^2 / 12 pi
-    # a := exp(-v^2/2a^2) = exp(-m v^2 / 2 kT) -> a = sqrt(kt/m) = sqrt(B^2/12 pi m)
-    maxwell = stats.maxwell(
-    
+def MJ(e,gamma,beta):
+    #returns the probability of finding energy e in a MJ distribution
+    theta=.41 # =kt/mc^2
+    const = .0542868 # = theta * K_2(1/theta)
+    return const*beta*gamma**2*math.exp(-gamma/theta)
 
+def MJCDF(e,gamma,beta):
+    # returns CDF of the MJ PDF
+    return quad(MJ(x,gamma,beta),1,e,args=(x))[0]
+    
+        
+def get_electron():
+    #return list of gamma,beta for n electrons
+    #thermal distribution with 3kT n/2 = B^2 /8pi -> kT = B^2 / 12 pi n
+    # a := exp(-v^2/2a^2) = exp(-m v^2 / 2 kT) -> a = sqrt(kt/m) = sqrt(B^2/12 pi m n)
+    # magnetic field in corona: http://mnras.oxfordjournals.org/content/early/2013/01/17/mnras.sts574.abstract
+    # B ~ 10^5 G
+    # electron density
+    # T = 10^9 K
+    # k = 1.38 E-16 erg/K
+    # a = sqrt(kt/m) = 1.23E10 cm/s
+    c = 3E10
+    maxwell = stats.maxwell(scale=1.23E10)
+    v = maxwell.rvs(size=1)
+    if v<=c:
+        gamma=1/math.sqrt(1-(v/c)**2)
+        beta = math.sqrt(1-gamma**-2)
+    return gamma, beta
+    
 
 def main():
     #constants (CGS)
     m=1E-27
     c=3E10
-    E=1.6E-6
-    gamma=E/(m*c**2)
-    beta=math.sqrt(1-gamma**-2)
+    #E=1.6E-6
+    #gamma=E/(m*c**2)
+    #beta=math.sqrt(1-gamma**-2)
 
     print 'gamma, beta, emax/e: ',gamma,beta,(gamma*(1+beta))**2
     niter=int(10**args.N)
     print 'Producing 1E'+str(args.N)+' photons'
 
+    gammalist,betalist = get_electron(100000)
+    
         
     #initialize stuff
     disc=0
